6d18659cfbe7f9fbda61babab4a06ed46c923f0f
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-06-08 13:55:59 -0300

    agrego wow min a js

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// Almaceno valores
+
+let número;
+
+// Asigno un dato a una vriable 
+
+número = 2023; // Número almacena La - placita - Pañalera
+
+
+// Datos
+
+let frase;
+frase = "La placita - Pañalera";
+
+// Inicializo variables
+
+let numero = 2023;
+
+// Constante
+
+const PI = 6;
+
+// Operación con strings
+
+let texto1 = 'La placita';
+let texto2 = ' - Pañalera';
+
+let texto3 = texto1 + texto2; // 'La placita - Pañalera'
+
+// Visualización de datos en la consola
+let numero3 = 2023;
+let txt = 'La placita - Pañalera'
+
+console.log(txt);
+console.log(numero3);
+
+// Agrego popup
+
+alert('Te vamos a pedir que ingreses el correo electronico');
+
+// Agrego PROMPT
+
+let correo;
+
+correo = prompt ('Ingrese su correo electrónico');
+
+console.log(correo);
+
+// Tipo de dato booleano
+
+let dato = false; // Tipo de dato booleano
+dato = true; // tipo de dato booleano
+
+if (true) {
+    console.log
+}
