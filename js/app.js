38dc725859b0cc05e6f58a0000f8bf1bdf85bda8
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-06-08 19:08:29 -0300

    agrego cambios a js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ let número;
 
 número = 2023; // Número almacena La - placita - Pañalera
 
-
 // Datos
 
 let frase;
@@ -34,23 +33,65 @@ let txt = 'La placita - Pañalera'
 console.log(txt);
 console.log(numero3);
 
+
+// Declaración de la función 
+function saludar () {
+    alert('Hola como va!')
+}
+
 // Agrego popup
 
 alert('Te vamos a pedir que ingreses el correo electronico');
 
-// Agrego PROMPT
+let correo = prompt('Ingrese su correo electronico');
 
-let correo;
-
-correo = prompt ('Ingrese su correo electrónico');
-
-console.log(correo);
+if (correo) {
+    correo = prompt('Ingrese su correo electrónico');
+    console.log('Verificando correo electrónico');
+}else {
+    alert('Por favor ingresar correo electrónico en el caso de no haberlo hecho');
+}
 
 // Tipo de dato booleano
 
-let dato = false; // Tipo de dato booleano
-dato = true; // tipo de dato booleano
+let dato = true; // Tipo de dato booleano
+dato = false; // tipo de dato booleano
 
 if (true){
     console.log('Correo ingresado')
 }
+
+// Operadores de comparación
+
+let a = 'Correo ingresado';
+let b = 'Correo ingresado';
+let resultado = a == b;
+
+console.log(resultado);
+
+if (a == b) {
+    console.log('Correo ingresado')
+}
+
+// Estrictamente distinto
+// resultado = a !== b;
+// console.log(resultado);
+
+resultado = a !== b;
+console.log(resultado);
+
+if (a !==b) {
+    console.log('Son distintos')
+}
+
+// Para que necesita comprar en la pañalera
+
+let necesidad = prompt ('Ingrese ingrese la palabra Madre si es para su hijo o ingrese la palabra Adulto si es para algun familiar')
+const Madre = 'Madres que necesitan pañales o accesorios para bebés';
+
+if (necesidad >= Madre) {
+    console.log ('Le mostraremos nuestros productos para bebés') 
+}else {
+    console.log('Le mostraremos productos para adultos')
+}
+
